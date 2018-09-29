3bd1669ef06a72963ede63617acd3bccdca72ec9
qqycc <963933327@qq.com>
2018-09-29 12:59:59 +0800

    第二章

---
+++ c_primer/c_primer/exercise1.c
@@ -0,0 +1,37 @@
+//练习2.1
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	printf("Qin qianyu\n");
+	printf("Qin\nqianyu\n");
+	printf("Qin");
+	printf(" qianyu\n");
+	
+	system("pause");
+	return  0;
+}
+
+//练习2.2  打印姓名和地址
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main(){
+//	printf("Qin qianyu\n");
+//	printf("Shanxi  xi'an\n");
+//
+//	system("pause");
+//	return  0;
+//}
+
+//练习2.3  把年龄转换成天数，并显示这两个值
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main(){
+//	int age = 20;
+//	int day = age * 365;
+//	printf("I am %d years,It's %d days.\n", age, day);
+//	system("pause");
+//	return  0;
+//}
