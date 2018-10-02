77c49568d94cb287bdf42ccad13b1f79d8894821
qqycc <963933327@qq.com>
2018-10-02 23:32:38 +0800

    第三章

--- c_primer/c_primer/exercise1.c
+++ c_primer/c_primer/exercise1.c
@@ -117,11 +117,11 @@
 //
 //	Smile();
 //	printf("\n");
-//    system("pause");
+//	system("pause");
 //	return 0;
 //}
 
-//练习2.8
+////练习2.8
 //#include <stdio.h>
 //#include <stdlib.h>
 //
@@ -137,8 +137,135 @@
 //	printf("starting now：\n");
 //	one_three();
 //	two();
-//	printf("three\n");
-//	printf("done！\n");
+//	printf("three\n  done！\n");
+//	system("pause");
+//	return 0;
+//}
+
+////例题3.3
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main(){
+//	int num = 100;
+//
+//	printf("10的十进制表示为%d,十六进制表示为%x，八进制表示为%o\n", num, num, num);
+//	printf("10的十进制表示为%d,十六进制表示为%#x，八进制表示为%#o\n", num, num, num);
+//
+//	system("pause");
+//	return 0;
+//}
+
+////例题3.4
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main(){
+//	unsigned int un = 3000000000;
+//	short end = 200;
+//	long big = 65537;
+//	long long verybig = 12345678905896;
+//
+//	printf("un=%u and not %d\n", un, un);
+//	printf("end=%hd and not %d\n", end, end);
+//	printf("big=%ld and not %hd\n", big, big);
+//	printf("verybig=%lld and not %ld\n", verybig, verybig);
+//
+//	system("pause");
+//	return 0;
+//}
+
+//练习3.2
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main(){
+//	int ASCII;
+//
+//	printf("请输入一个ASCII码：");
+//	scanf("%d",& ASCII);
+//
+//	printf("\n所输入ASCII码对应的字符为：%c\n", ASCII);
+//	system("pause");
+//	return 0;
+//}
+
+//练习3.3
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main(){
+//	printf("\a Startled by the sudden sound,Sally shouted,\n");
+//	printf("\"By the Great Pumpkin, what was that！\"\n");
+//
+//	system("pause");
+//	return 0;
+//}
+
+//练习3.4
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main(){
+//	float f;
+//	printf("Enter a floating-point value:");
+//	scanf("%f", &f);
+//
+//	printf("\nfixed-point notation:%f\n", f);
+//	printf("exponential notation:%e\n", f);
+//	printf("p notation:%a\n", f);
+//
+//	system("pause");
+//	return 0;
+//}
+
+//练习3.5
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main(){
+//	int age;
+//	printf("请输入您的年龄：");
+//	scanf("%d", &age);
+//
+//	printf("\n您的年龄所对应的秒数为：%d\n", age*31560000);
+//	system("pause");
+//	return 0;
+//}
+
+//练习3.6
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int quart;
+
+	printf("请输入水的夸脱数:");
+	scanf("%d", quart);
+	printf("\n");
+
+	//1quart=950g,m水=3.0*10^-23
+	//水分子数=quart*950/（3.0*10^-23）
+	int m = 3.0*pow(10, -23);
+	printf("水分子的数量为：%d", quart * 950 / m);
+	system("pause");
+	return 0;
+}
+
+
+
+
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main(){
+//
 //	system("pause");
 //	return 0;
 //}
