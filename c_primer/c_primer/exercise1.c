b6cb0243889052a83b534e577571e36a153583f9
qqycc <963933327@qq.com>
2018-10-05 22:22:46 +0800

    第五章1

--- c_primer/c_primer/exercise1.c
+++ c_primer/c_primer/exercise1.c
@@ -492,7 +492,26 @@
 //}
 
 //练习5.1
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(){
+	const int minperhour = 60;
+	int minutes, hours, mins;
+
+	printf("请输入分钟数：");
+		scanf("%d",&minutes);
+	while (minutes > 0){
+		hours = minutes / minperhour;
+		mins = minutes%minperhour;
+		printf("%d minutes=%d hours,%d minutes\n", minutes, hours, mins);
+		scanf("%d", &minutes);
+	}
+
+	system("pause");
+	return 0;
+}
 
 
 
