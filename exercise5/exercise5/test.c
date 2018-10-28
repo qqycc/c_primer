b86bd8583ae2b603c1be01f86e3c518c47d27ab7
qqycc <963933327@qq.com>
2018-10-28 17:48:17 +0800

    第五章1.2.3

---
+++ exercise5/exercise5/test.c
@@ -0,0 +1,84 @@
+//1.
+//#define  _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//#define time 60
+//
+//int main(){
+//	while (1){
+//		int minute = 0;
+//		printf("请输入分钟数：");
+//		scanf("%d", &minute);
+//		if (minute <= 0){
+//			break;
+//		}
+//		int hour = minute / time;
+//		int min = minute - hour*time;
+//		printf("%d分钟=%d小时%d分钟\n", minute, hour, min);
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//2.
+//#define  _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main(){
+//	int num;
+//	printf("请输入一个数：\n");
+//	scanf("%d", &num);
+//	int i = num;
+//	for (num; num < (i + 11); num++){
+//		printf("%d ", num);
+//	}
+//	printf("\n");
+//	system("pause");
+//	return 0;
+//}
+
+//3.
+//#define  _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main(){
+//	while (1){
+//		int day = 0;
+//		printf("请输入天数：\n");
+//		scanf("%d", &day);
+//		if (day <= 0){
+//			break;
+//		}
+//		int week = day / 7;
+//		int days = day-week * 7 ;
+//		printf("%d days are %d weeks,%d days\n",day,week,days);
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//4.
+#define  _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+
+	system("pause");
+	return 0;
+}
+
+//#define  _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main(){
+//
+//
+//	system("pause");
+//	return 0;
+//}
